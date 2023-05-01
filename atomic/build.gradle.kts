84f67b311e93e7262f9a62a34b96895c9aa18ff2
Sphericalkat <amolele@gmail.com>
2023-05-01 06:14:29 +0530

    feat(ci): add ability to publish to github maven repository
    Signed-off-by: Sphericalkat <amolele@gmail.com>

--- atomic/build.gradle.kts
+++ atomic/build.gradle.kts
@@ -1,9 +1,25 @@
+import java.net.URI
+
 plugins {
     id("java")
     id("java-library")
+    id ("maven-publish")
 }
 
 group = "jp.co.goalist"
+
+publishing {
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/SphericalKat/atomic-demo")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
