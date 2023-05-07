fc7e4ec9f009eb146cd6c2cffa25bd2ead29cd9e
Sphericalkat <amolele@gmail.com>
2023-05-07 19:01:56 +0530

    fix(localbus): check for existing event flows when collecting
    Signed-off-by: Sphericalkat <amolele@gmail.com>

--- atomic/src/main/kotlin/jp/co/goalist/utils/Events.kt
+++ atomic/src/main/kotlin/jp/co/goalist/utils/Events.kt
@@ -3,14 +3,17 @@ package jp.co.goalist.utils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import kotlin.coroutines.CoroutineContext
 
 
 data class Event(
-    val name: String,
     val data: Any
 )
 
@@ -18,14 +21,26 @@ class LocalBus(override val coroutineContext: CoroutineContext = Job()) : Corout
     private val flows = mutableMapOf<String, MutableSharedFlow<Event>>()
 
     fun on(eventName: String, callBack: (data: Event) -> Unit) {
-        val flow = MutableSharedFlow<Event>(replay = 0)
-        flow.onEach { callBack(it) }
+        val existingFlow = flows[eventName]
+        if (existingFlow == null) {
+            val flow = MutableSharedFlow<Event>(replay = 0)
+            flow.onEach { callBack(it) }.launchIn(this)
 
-        flows[eventName] = flow
+            flows[eventName] = flow
+            logger.debug("Registered new event $eventName")
+        } else {
+            existingFlow.onEach { callBack(it) }.launchIn(this)
+            logger.debug("Registered new event handler on existing event $eventName")
+        }
     }
 
     fun emit(eventName: String, data: Any) {
         val flow = flows[eventName]
-        runBlocking { flow?.emit(Event(eventName, data)) }
+        runBlocking { flow?.emit(Event(data)) }
+        logger.debug("Emitted event $eventName")
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(LocalBus::class.java)
     }
 }
